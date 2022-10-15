26aad3a74864877696c8fd8e1b9e2d10916aa877
Natalia Lopez <natalia.lopez17@upr.edu>
2022-10-14 20:51:08 -0400

    working on making the pages more responsive.
    POST request should be done.

--- react_native/src/screens/root_screens/RootStackScreenComponent.js
+++ react_native/src/screens/root_screens/RootStackScreenComponent.js
@@ -1,10 +1,10 @@
 import React from "react";
-import {createNativeStackNavigator} from "@react-navigation/native-stack";
-import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
-import {Dimensions, ImageBackground, StyleSheet, Text} from "react-native";
-import Icon from 'react-native-vector-icons/Ionicons';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Dimensions, ImageBackground, StyleSheet, Text } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
 import backgroundLight from "../../../assets/images/backgroundLight.png";
-import * as Animatable from "react-native-animatable"
+import * as Animatable from "react-native-animatable";
 import SplashScreenComponent from "./SplashScreenComponent";
 import SignInScreenComponent from "./SignInScreenComponent";
 import SignUpScreenComponent from "./SignUpScreenComponent";
@@ -16,63 +16,125 @@ const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 function RootStackScreenComponent() {
-    const loggedIn = true;
-    return (
-        <ImageBackground source={backgroundLight} resizeMode="cover" style={{ width: "100%", flex: 1, justifyContent: "center" }}>
-            {!loggedIn ?
-                <Stack.Navigator screenOptions={{headerShown: false}}>
-                    <Stack.Screen name={"SplashScreen"} component={SplashScreenComponent}/>
-                    <Stack.Screen name={"SignIn"} component={SignInScreenComponent}  options={{headerBackTitle: "Back"}}/>
-                    <Stack.Screen name={"SignUp"} component={SignUpScreenComponent}  options={{headerBackTitle: "Back"}}/>
-                </Stack.Navigator> :
-                <Animatable.View animation={"fadeInUp"} style={{flex: 1}}>
-                    <Tab.Navigator
-                        initialRouteName="Home"
-                        screenOptions={{
-                            backgroundColor: "#ffff",
-                            showLabel: false,
-                            tabBarStyle: {
-                                position: "absolute",
-                                alignItems: "center",
-                                backgroundColor: "#ffffff",
-                                height: 90,
-                            }
-                        }}
-                    >
-                        <Tab.Screen
-                            name={"Home"}
-                            component={HomeScreenComponent}
-                            options={{
-                                headerShown: false,
-                                tabBarLabel: ({focused}) => (<Text style={{fontSize: 10, color: focused ? "#000000" : "#696969"}}>Home</Text>),
-                                tabBarIconStyle: {top: 5},
-                                tabBarIcon: ({focused}) => (<Icon name={'home'} color={focused ? "#000000" : "#696969"} size={26}/>)
-                            }}
-                        />
-                        <Tab.Screen
-                            name={"Wallet"}
-                            component={WalletScreenComponent}
-                            options={{
-                                headerShown: false,
-                                tabBarLabel: ({focused}) => (<Text style={{fontSize: 10, color: focused ? "#000000" : "#696969"}}>Wallet</Text>),
-                                tabBarIconStyle: {top: 5},
-                                tabBarIcon: ({focused}) => (<Icon name={'wallet'} color={focused ? "#000000" : "#696969"} size={26}/>)
-                            }}
-                        />
+  const loggedIn = true;
+  return (
+    <ImageBackground
+      source={backgroundLight}
+      resizeMode="cover"
+      style={{ width: "100%", flex: 1, justifyContent: "center" }}
+    >
+      {loggedIn ? ( // manual toggle for now to test
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen
+            name={"SplashScreen"}
+            component={SplashScreenComponent}
+          />
+          <Stack.Screen
+            name={"SignIn"}
+            component={SignInScreenComponent}
+            options={{ headerBackTitle: "Back" }}
+          />
+          <Stack.Screen
+            name={"SignUp"}
+            component={SignUpScreenComponent}
+            options={{ headerBackTitle: "Back" }}
+          />
+        </Stack.Navigator>
+      ) : (
+        <Animatable.View animation={"fadeInUp"} style={{ flex: 1 }}>
+          <Tab.Navigator
+            initialRouteName="Home"
+            screenOptions={{
+              backgroundColor: "#ffff",
+              showLabel: false,
+              tabBarStyle: {
+                position: "absolute",
+                alignItems: "center",
+                backgroundColor: "#ffffff",
+                height: 90,
+              },
+            }}
+          >
+            <Tab.Screen
+              name={"Home"}
+              component={HomeScreenComponent}
+              options={{
+                headerShown: false,
+                tabBarLabel: ({ focused }) => (
+                  <Text
+                    style={{
+                      fontSize: 10,
+                      color: focused ? "#000000" : "#696969",
+                    }}
+                  >
+                    Home
+                  </Text>
+                ),
+                tabBarIconStyle: { top: 5 },
+                tabBarIcon: ({ focused }) => (
+                  <Icon
+                    name={"home"}
+                    color={focused ? "#000000" : "#696969"}
+                    size={26}
+                  />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name={"Wallet"}
+              component={WalletScreenComponent}
+              options={{
+                headerShown: false,
+                tabBarLabel: ({ focused }) => (
+                  <Text
+                    style={{
+                      fontSize: 10,
+                      color: focused ? "#000000" : "#696969",
+                    }}
+                  >
+                    Wallet
+                  </Text>
+                ),
+                tabBarIconStyle: { top: 5 },
+                tabBarIcon: ({ focused }) => (
+                  <Icon
+                    name={"wallet"}
+                    color={focused ? "#000000" : "#696969"}
+                    size={26}
+                  />
+                ),
+              }}
+            />
 
-                        <Tab.Screen
-                            name={"Account"}
-                            component={AccountScreenComponent}
-                            options={{
-                                headerShown: false,
-                                tabBarLabel: ({focused}) => (<Text style={{fontSize: 10, color: focused ? "#000000" : "#696969"}}>Account</Text>),
-                                tabBarIconStyle: {top: 5},
-                                tabBarIcon: ({focused}) => (<Icon name={'ios-person'} color={focused ? "#000000" : "#696969"} size={26}/>)
-                            }}
-                        />
-                    </Tab.Navigator>
-                </Animatable.View>}
-        </ImageBackground>
-    );
+            <Tab.Screen
+              name={"Account"}
+              component={AccountScreenComponent}
+              options={{
+                headerShown: false,
+                tabBarLabel: ({ focused }) => (
+                  <Text
+                    style={{
+                      fontSize: 10,
+                      color: focused ? "#000000" : "#696969",
+                    }}
+                  >
+                    Account
+                  </Text>
+                ),
+                tabBarIconStyle: { top: 5 },
+                tabBarIcon: ({ focused }) => (
+                  <Icon
+                    name={"ios-person"}
+                    color={focused ? "#000000" : "#696969"}
+                    size={26}
+                  />
+                ),
+              }}
+            />
+          </Tab.Navigator>
+        </Animatable.View>
+      )}
+    </ImageBackground>
+  );
 }
 export default RootStackScreenComponent;
--- react_native/src/screens/root_screens/SignUpScreenComponent.js
+++ react_native/src/screens/root_screens/SignUpScreenComponent.js
@@ -1,207 +1,278 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import {
-    Dimensions,
-    Image,
-    KeyboardAvoidingView,
-    Platform, SafeAreaView,
-    StatusBar,
-    StyleSheet,
-    Text, TextInput, TouchableOpacity,
-    View
+  Dimensions,
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
 } from "react-native";
+import { scale, verticalScale, moderateScale } from "react-native-size-matters";
 import Feather from "react-native-vector-icons/Feather";
-import * as Animatable from 'react-native-animatable'
+import * as Animatable from "react-native-animatable";
 import paw from "../../../assets/images/paw.png";
 import ActionButtonComponent from "../../components/ActionButtonComponent";
 
-function SignUpScreenComponent({navigation}){
-    const [name, setName] = useState("");
-    const [username, setUsername] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+var deviceWidth = Dimensions.get("window").width;
+var deviceHeight = Dimensions.get("window").height;
 
-    const [showPassword, setShowPassword] = useState(false);
-    const [isValidPassword, setIsValidPassword] = useState(true);
+function SignUpScreenComponent({ navigation }) {
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleShowPassword = () => {
-        setShowPassword(!showPassword);
-    };
+  const [showPassword, setShowPassword] = useState(false);
+  const [isValidPassword, setIsValidPassword] = useState(true);
 
-    const handleSignUp = () => {
+  const [isLoading, setIsLoading] = useState(false);
 
-    };
+  const REGISTER_URL = "ec2-52-200-5-135.compute-1.amazonaws.com";
 
-    return(
-        <SafeAreaView>
-            <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : null} style={styles.container}>
-                <StatusBar backgroundColor={"rgba(6, 144, 68, 1)"} barStyle={"light-content"}/>
-                <Animatable.View animation={"slideInDown"} style={styles.title}>
-                    <View style={{flexDirection: "row"}}>
-                        <Text style={{
-                            color: "white",
-                            fontSize: Platform.OS === "ios" ? 64 : 53,
-                            textShadowColor: 'rgba(0, 0, 0, 0.75)',
-                            textShadowOffset: { width: 0, height: 3 },
-                            textShadowRadius: 10,
-                            position: "absolute"
-                        }}> Tüter </Text>
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
 
-                        <Image
-                            source={paw}
-                            style={{
-                                marginTop: "23%",
-                                marginLeft: "60%",
-                                height: 24,
-                                width: 24,
-                            }}
-                            resizeMode={"contain"}
-                        />
-                    </View>
-                </Animatable.View>
-                <View style={{padding: 10}}/>
-                <Animatable.View animation={"fadeInUpBig"} style={[styles.footer, {backgroundColor: "#ffffff"}]}>
-                    <View style={{alignItems:"center", paddingBottom: 39}}>
-                        <Text style={{fontSize: 34}}>Sign Up</Text>
-                    </View>
+  const handleSignUp = async (event) => {
+    if (!username.trim() || !email.trim() || !name.trim()) {
+      alert("Name or Email is invalid");
+      return;
+    }
+    setIsLoading(true);
+    try {
+      const response = await axios.post(
+        `${REGISTER_URL}/tuter/users`,
+        JSON.stringify({ username, password, email, name }),
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      );
+      if (response.status === 201) {
+        alert(` You have created: ${JSON.stringify(response.data)}`);
+        setIsLoading(false);
+        setUsername("");
+        setEmail("");
+        setPassword("");
+        setName("");
+      } else {
+        throw new Error("An error has occurred");
+      }
+    } catch (error) {
+      alert("An error has occurred");
+      setIsLoading(false);
+    }
+  };
 
-                    <Text style={styles.text_footer}>Name</Text>
-                    <View style={[styles.action, {paddingRight: 5}]}>
-                        <TextInput
-                            autoCapitalize={'none'}
-                            placeholder={"Enter your name"}
-                            clearButtonMode={"while-editing"}
-                            placeholderTextColor={"rgba(0,0,0,0.45)"}
-                            style={[styles.textInput]}
-                            onChangeText={(name) => setName(name)}
-                        />
-                    </View>
+  return (
+    <SafeAreaView>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : null}
+        style={styles.container}
+      >
+        <StatusBar
+          backgroundColor={"rgba(6, 144, 68, 1)"}
+          barStyle={"light-content"}
+        />
+        <Animatable.View animation={"slideInDown"} style={styles.title}>
+          <View style={styles.titleBar}>
+            <Text style={styles.tuter}> Tüter </Text>
 
-                    <Text style={[styles.text_footer, {marginTop: 25}]}>Username</Text>
-                    <View style={[styles.action, {paddingRight: 5}]}>
-                        <TextInput
-                            autoCapitalize={'none'}
-                            placeholder={"Enter your username"}
-                            clearButtonMode={"while-editing"}
-                            placeholderTextColor={"rgba(0,0,0,0.45)"}
-                            style={[styles.textInput]}
-                            onChangeText={(username) => setUsername(username)}
-                        />
-                    </View>
+            <Image
+              source={paw}
+              style={styles.pawImage}
+              resizeMode={"contain"}
+            />
+          </View>
+        </Animatable.View>
+        <View style={{ padding: 10 }} />
+        {/* <Animatable.View
+          animation={"fadeInUpBig"}
+          style={[styles.footer, { backgroundColor: "#ffffff" }]}
+        >
+          <View style={{ alignItems: "center", paddingBottom: 39 }}>
+            <Text style={{ fontSize: 34 }}>Sign Up</Text>
+          </View>
 
-                    <Text style={[styles.text_footer, {marginTop: 25}]}>Email</Text>
-                    <View style={[styles.action, {paddingRight: 5}]}>
-                        <TextInput
-                            autoCapitalize={'none'}
-                            placeholder={"Enter your email"}
-                            clearButtonMode={"while-editing"}
-                            placeholderTextColor={"rgba(0,0,0,0.45)"}
-                            style={[styles.textInput]}
-                            onChangeText={(email) => setEmail(email)}
-                        />
-                    </View>
-                    <Text style={[styles.text_footer, {marginTop: 25}]}>Password</Text>
-                    <View style={styles.action}>
-                        <TextInput
-                            autoCapitalize={'none'}
-                            secureTextEntry={showPassword}
-                            placeholder={"Enter your password"}
-                            clearButtonMode={"while-editing"}
-                            placeholderTextColor={"rgba(0,0,0,0.45)"}
-                            style={[styles.textInput]}
-                            onChangeText={
-                                (pass) => {
-                                    if(pass.trim().length >= 8) {
-                                        setPassword(pass);
-                                        setIsValidPassword(true);
-                                    } else {
-                                        setPassword(pass);
-                                        setIsValidPassword(!pass.trim().length > 0);
-                                    }
-                                }
-                            }
-                            onEndEditing={() => {
-                                if(password.trim().length < 8 && password.trim().length > 0) setIsValidPassword(false);
-                                else setIsValidPassword(true);
-                            }}
-                        />
-                        <View style={{padding: 5}}>
-                            <TouchableOpacity onPress={handleShowPassword}>
-                                {showPassword === true && <Feather name="eye-off" color={"rgba(0,0,0,0.45)"} size={20}/>}
-                                {showPassword === false && <Feather name="eye" color={"rgba(0,0,0,0.45)"} size={20}/>}
-                            </TouchableOpacity>
-                        </View>
-                    </View>
-                    {isValidPassword ? null :
-                        <Animatable.View animation={"fadeInLeft"} duration={500}>
-                            <Text style={styles.errorMsg}>Password must be at least 8 characters long</Text>
-                        </Animatable.View>
-                    }
-                    <View style={{alignItems: "center", paddingTop: 55, paddingBottom: "50%"}}>
-                        <ActionButtonComponent
-                            label={"Sign Up"}
-                            labelColor={"#ffffff"}
-                            buttonColor={"#069044"}
-                            width={122}
-                            height={48}
-                            bold={true}
-                            onPress={() => handleSignUp()}
-                        />
-                    </View>
-                </Animatable.View>
-            </KeyboardAvoidingView>
-        </SafeAreaView>
-    );
+          <Text style={styles.text_footer}>Name</Text>
+          <View style={[styles.action, { paddingRight: 5 }]}>
+            <TextInput
+              autoCapitalize={"none"}
+              placeholder={"Enter your name"}
+              clearButtonMode={"while-editing"}
+              placeholderTextColor={"rgba(0,0,0,0.45)"}
+              style={[styles.textInput]}
+              onChangeText={(name) => setName(name)}
+            />
+          </View>
+
+          <Text style={[styles.text_footer, { marginTop: 25 }]}>Username</Text>
+          <View style={[styles.action, { paddingRight: 5 }]}>
+            <TextInput
+              autoCapitalize={"none"}
+              placeholder={"Enter your username"}
+              clearButtonMode={"while-editing"}
+              placeholderTextColor={"rgba(0,0,0,0.45)"}
+              style={[styles.textInput]}
+              onChangeText={(username) => setUsername(username)}
+            />
+          </View>
+
+          <Text style={[styles.text_footer, { marginTop: 25 }]}>Email</Text>
+          <View style={[styles.action, { paddingRight: 5 }]}>
+            <TextInput
+              autoCapitalize={"none"}
+              placeholder={"Enter your email"}
+              clearButtonMode={"while-editing"}
+              placeholderTextColor={"rgba(0,0,0,0.45)"}
+              style={[styles.textInput]}
+              onChangeText={(email) => setEmail(email)}
+            />
+          </View>
+          <Text style={[styles.text_footer, { marginTop: 25 }]}>Password</Text>
+          <View style={styles.action}>
+            <TextInput
+              autoCapitalize={"none"}
+              secureTextEntry={showPassword}
+              placeholder={"Enter your password"}
+              clearButtonMode={"while-editing"}
+              placeholderTextColor={"rgba(0,0,0,0.45)"}
+              style={[styles.textInput]}
+              onChangeText={(pass) => {
+                if (pass.trim().length >= 8) {
+                  setPassword(pass);
+                  setIsValidPassword(true);
+                } else {
+                  setPassword(pass);
+                  setIsValidPassword(!pass.trim().length > 0);
+                }
+              }}
+              onEndEditing={() => {
+                if (password.trim().length < 8 && password.trim().length > 0)
+                  setIsValidPassword(false);
+                else setIsValidPassword(true);
+              }}
+            />
+            <View style={{ padding: 5 }}>
+              <TouchableOpacity onPress={handleShowPassword}>
+                {showPassword === true && (
+                  <Feather
+                    name="eye-off"
+                    color={"rgba(0,0,0,0.45)"}
+                    size={20}
+                  />
+                )}
+                {showPassword === false && (
+                  <Feather name="eye" color={"rgba(0,0,0,0.45)"} size={20} />
+                )}
+              </TouchableOpacity>
+            </View>
+          </View>
+          {isValidPassword ? null : (
+            <Animatable.View animation={"fadeInLeft"} duration={500}>
+              <Text style={styles.errorMsg}>
+                Password must be at least 8 characters long
+              </Text>
+            </Animatable.View>
+          )}
+          <View style={styles.signupButton}>
+            <ActionButtonComponent
+              label={"Sign Up"}
+              labelColor={"#ffffff"}
+              buttonColor={"#069044"}
+              width={122}
+              height={48}
+              bold={true}
+              onPress={() => handleSignUp()}
+            />
+          </View>
+        </Animatable.View> */}
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        height: Dimensions.get("screen").height,
-        justifyContent: "center",
-        paddingTop: "12.5%"
-    },
-    title: {
-        position: 'relative',
-        left: '30%',
-        right: '50%',
-        width: 234,
-        height: 74,
-        paddingTop: "75%",
-        paddingBottom: "25%"
-    },
-    footer:{
-        backgroundColor: "#ffff",
-        borderTopLeftRadius: 30,
-        borderTopRightRadius: 30,
-        paddingHorizontal: 20,
-        paddingVertical: 30,
-        paddingBottom: "60%"
-    },
-    errorMsg: {
-        marginTop: 10,
-        color: 'red'
-    },
-    textInput: {
-        flex: 1,
-        marginTop: Platform.OS === 'ios' ? 0 : -12,
-        paddingLeft: 10,
-        color: "#05375a"
-    },
-    action:{
-        flexDirection: "row",
-        height: 44,
-        marginTop:10,
-        borderRadius: 10,
-        borderWidth: 1.5,
-        borderColor: "#000000",
-        padding: 5,
-        paddingRight: 5,
-        shadowRadius: 10,
-        shadowOffset: {width: 0, height: 3},
-        shadowColor: "rgba(0,0,0,0.75)"
-    },
-    text_footer:{
-        color: "#05375a",
-        fontSize: 18
-    },
-})
+  container: {
+    height: Dimensions.get("screen").height,
+    width: Dimensions.get("screen").width,
+    paddingTop: "12.5%",
+    justifyContent: "center",
+  },
+  title: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  titleBar: {
+    flexDirection: "row",
+    flex: 2,
+    height: "50%",
+    justifyContent: "center",
+    backgroundColor: "#e7622e",
+    borderColor: "#fcfdfb",
+    borderWidth: 2,
+  },
+  tuter: {
+    color: "white",
+    justifyContent: "center",
+    fontSize: scale(40),
+    textShadowColor: "rgba(0, 0, 0, 0.75)",
+    textShadowOffset: { width: 0, height: 3 },
+    textShadowRadius: 10,
+    marginLeft: moderateScale(100, 0.3),
+  },
+  pawImage: {
+    flex: 1,
+    marginTop: verticalScale(45, 0.3),
+    width: moderateScale(22, 0.3),
+    height: verticalScale(22, 0.3),
+    marginLeft: moderateScale(10, 0.3),
+  },
+  footer: {
+    backgroundColor: "#ffff",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+    paddingBottom: "60%",
+  },
+  errorMsg: {
+    marginTop: 10,
+    color: "red",
+  },
+  textInput: {
+    flex: 1,
+    marginTop: Platform.OS === "ios" ? 0 : -12,
+    paddingLeft: 10,
+    color: "#05375a",
+  },
+  signupButton: {
+    alignItems: "center",
+    paddingTop: "10%",
+    paddingBottom: "50%",
+  },
+  action: {
+    flexDirection: "row",
+    height: 44,
+    marginTop: 10,
+    borderRadius: 10,
+    borderWidth: 1.5,
+    borderColor: "#000000",
+    padding: 5,
+    paddingRight: 5,
+    shadowRadius: 10,
+    shadowOffset: { width: 0, height: 3 },
+    shadowColor: "rgba(0,0,0,0.75)",
+  },
+  text_footer: {
+    color: "#05375a",
+    fontSize: 18,
+  },
+});
 export default SignUpScreenComponent;

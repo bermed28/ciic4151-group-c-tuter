e02a27bf4d00ad3dd0c48e710e0d89d23ee4c54c
Natalia Lopez <natalia.lopez17@upr.edu>
2022-10-14 20:51:37 -0400

    fixes??

--- app.py
+++ app.py
@@ -8,18 +8,22 @@ import json
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return "<h1>Hola Hovito<h1/>"
 
+
 """""""""""""MAIN ENTITY HANDLERS (CRUD Operations)"""""""""""""""
+
 
 @app.route('/tuter/users', methods=['GET', 'POST'])
 def handleUsers():
-    if request.method == 'POST': #ADD
+    if request.method == 'POST':  # ADD
         return BaseUser().addNewUser(request.json)
     else:
-        return BaseUser().getAllUsers() #Get list of all users
+        return BaseUser().getAllUsers()  # Get list of all users
+
 
 @app.route('/tuter/users/<int:uid>', methods=['GET', 'PUT', 'DELETE'])
 def handleUsersbyId(uid):
@@ -30,10 +34,12 @@ def handleUsersbyId(uid):
     elif request.method == 'DELETE':
         return BaseUser().deleteUser(uid)
 
+
 @app.route('/tuter/users/usernames', methods=['POST'])
 def handleUsernames():
     if request.method == 'POST':
         return BaseUser().getRequestedIds(request.json)
+
 
 @app.route('/tuter/user-schedule', methods=['GET', 'POST'])
 def handleUserSchedules():
@@ -41,6 +47,7 @@ def handleUserSchedules():
         return BaseUserSchedule().addNewUserSchedule(request.json)
     else:
         return BaseUserSchedule().getAllUserSchedules()
+
 
 @app.route('/tuter/user-schedule/<int:usid>', methods=['GET', 'PUT', 'DELETE'])
 def handleUserSchedulebyId(usid):
@@ -51,6 +58,7 @@ def handleUserSchedulebyId(usid):
     elif request.method == 'DELETE':
         return BaseUserSchedule().deleteUserSchedule(usid)
 
+
 @app.route('/tuter/user-schedule/markunavailable', methods=['POST'])
 def handlemarkUserUnavailable():
     print(request.json)
@@ -60,13 +68,16 @@ def handlemarkUserUnavailable():
     else:
         return BaseUserSchedule().addNewUserSchedule(json.loads(request.data))
 
+
 @app.route('/tuter/user-schedule/markavailable', methods=['POST'])
 def handlemarkUserAvailable():
     return BaseUserSchedule().markAvailable(request.json)
 
+
 @app.route('/tuter/timeslots', methods=['GET'])
 def handleTimeSlots():
     return BaseTimeSlot().getAllTimeSlots()
+
 
 @app.route('/tuter/timeslots/<int:tid>', methods=['GET'])
 def handleTimeSlotbyId(tid):

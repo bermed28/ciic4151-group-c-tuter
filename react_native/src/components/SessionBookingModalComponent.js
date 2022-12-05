ce51f483bba56f19d9726ef774ffe3d1755b83de
Chris36021 <54044050+chris36021@users.noreply.github.com>
2022-12-04 20:47:17 -0400

    Fixed android modal styling

--- react_native/src/components/SessionBookingModalComponent.js
+++ react_native/src/components/SessionBookingModalComponent.js
@@ -277,7 +277,7 @@ function SessionBookingModalComponent(props) {
             }}>
                 <Animatable.View animation={"bounceIn"} style={{
                     width: "80%",
-                    height: "55%",
+                    height: Platform.OS === 'ios' ? "55%" : "62%",
                     backgroundColor: "#f2f2f7",
                     borderRadius: 10,
                     paddingBottom: "15%"

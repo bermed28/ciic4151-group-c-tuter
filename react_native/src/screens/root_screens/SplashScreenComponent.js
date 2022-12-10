f318fdd72c80a0b1e642234a4afd97dddd2b8dca
Fernando J. Bermúdez Medina <fernando.bermudez1@upr.edu>
2022-12-10 16:10:18 -0400

    Hid SIWA logs when revoking credentials
    Signed-off-by: Fernando J. Bermúdez Medina <fernando.bermudez1@upr.edu>

--- react_native/src/screens/root_screens/SplashScreenComponent.js
+++ react_native/src/screens/root_screens/SplashScreenComponent.js
@@ -133,7 +133,7 @@ function SplashScreenComponent({navigation}) {
     useEffect(() => {
         // onCredentialRevoked returns a function that will remove the event listener. useEffect will call this function when the component unmounts
         return appleAuth.onCredentialRevoked(async () => {
-            console.warn('If this function executes, User Credentials have been Revoked');
+            console.log('If this function executes, User Credentials have been Revoked');
         });
     }, []); // passing in an empty array as the second argument ensures this is only ran once when component mounts initially.
 
